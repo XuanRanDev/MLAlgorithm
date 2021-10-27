4a9940418bafaf0a7707bfc80ef62e7c3a9e2fd6
XuanRanDev <3135419729@qq.com>
2021-10-27 14:57:20 +0800

    Add lr var at LinearRegressionTest.py

--- GradingDesent/LinearRegressionTest.py
+++ GradingDesent/LinearRegressionTest.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+lr = 0.00001
 path = "data.txt"
 data = pd.read_csv(path, header=None)
 plt.scatter(data[:][0], data[:][1], marker='+')
